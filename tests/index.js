52b9a453b06a9849551cc6add04c32aade1fcc2c
Rogan Melo <roganmelo@gmail.com>
2019-02-03 17:24:49 -0300

    configure eslint-plugin-jest

--- tests/index.js
+++ tests/index.js
@@ -5,28 +5,30 @@ const obj = {
   b: [{ c: 'a' }]
 };
 
-test('It should return an error asking for a valid first parameter.', () => {
-  const invalidParameterError = new Error('You have to provide a valid first parameter.');
+describe('get-by-dot', () => {
+  it('should return an error asking for a valid first parameter.', () => {
+    const invalidParameterError = new Error('You have to provide a valid first parameter.');
 
-  expect(() => getByDot()).toThrowError(invalidParameterError);
-  expect(() => getByDot(1)).toThrowError(invalidParameterError);
-  expect(() => getByDot('obj')).toThrowError(invalidParameterError);
-  expect(() => getByDot(null)).toThrowError(invalidParameterError);
-});
+    expect(() => getByDot()).toThrowError(invalidParameterError);
+    expect(() => getByDot(1)).toThrowError(invalidParameterError);
+    expect(() => getByDot('obj')).toThrowError(invalidParameterError);
+    expect(() => getByDot(null)).toThrowError(invalidParameterError);
+  });
 
-test('It should return the given object if the path isn\'t passed.', () => {
-  expect(getByDot(obj)).toEqual(obj);
-});
+  it('should return the given object if the path isn\'t passed.', () => {
+    expect(getByDot(obj)).toEqual(obj);
+  });
 
-test('It should return path value for a given object.', () => {
-  expect(getByDot(obj, 'a.b')).toBe('c');
-});
+  it('should return path value for a given object.', () => {
+    expect(getByDot(obj, 'a.b')).toBe('c');
+  });
 
-test('It should return undefined for a path that doesn\'t exists.', () => {
-  expect(getByDot(obj, 'a.b.c')).toBe(undefined);
-});
+  it('should return undefined for a path that doesn\'t exists.', () => {
+    expect(getByDot(obj, 'a.b.c')).toBe(undefined);
+  });
 
-test('It should work with arrays.', () => {
-  expect(getByDot(obj, 'b.0.c')).toBe('a');
-  expect(getByDot(obj.b, '0.c')).toBe('a');
+  it('should work with arrays.', () => {
+    expect(getByDot(obj, 'b.0.c')).toBe('a');
+    expect(getByDot(obj.b, '0.c')).toBe('a');
+  });
 });
